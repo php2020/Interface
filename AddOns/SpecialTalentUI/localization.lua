962df882add9c12fd66f4a49907e842f9eb80e60
linyan <lin.yan@genecast.com.cn>
2023-10-13 12:09:07 +0800

    天赋模拟器

---
+++ AddOns/SpecialTalentUI/localization.lua
@@ -0,0 +1,20 @@
+SPECIAL_TALENT = "Special Talent";
+TALENT_POINTS = "Talent Points";
+UNSPENT_POINTS = "Unspent Points";
+TALENTS_LEARNED = "Learned";
+TALENTS_PLANNED = "Planned";
+SHIFT_CLICK_LEARN_TALENT = "Force Shift-click to Learn";
+PLANNED_RANK = "Planned Rank %d/%d";
+
+
+if ( GetLocale() == "zhCN" ) then
+    SPECIAL_TALENT = "天赋模拟器";
+    TALENT_POINTS = "已获得天赋点";
+    UNSPENT_POINTS = "未分配天赋点";
+    TALENTS_LEARNED = "已分配";
+    TALENTS_PLANNED = "模拟器";
+    RESET_PLANNED_TEMPLATE = "重置";
+    PLANNED_RANK = "模拟等级%d/%d";
+    LEARNED_RANK = "学会等级%d/%d";
+    SHIFT_CLICK_LEARN_TALENT = "按住shift强制学习";
+end
